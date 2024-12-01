2e22031c594686bc04912744032923b8a42a575c
Sourabh Jain <126811697+Jain131102@users.noreply.github.com>
2024-12-01 12:29:16 +0530

    Update WeightedSubstring.java

--- WeightedSubstring.java
+++ WeightedSubstring.java
@@ -46,3 +46,34 @@ public class WeightedSubstring {
         sc.close();
     }
 }
+
+
+// import java.util.*;
+// 	class WeightedSubstring {
+// 	static int distinctSubString(String P, String Q, int K, int N) {
+// 	HashSet < String > S = new HashSet < String > ();
+// 	for (int i = 0; i < N; ++i) {
+// 	int sum = 0;
+// 	String s = "";
+// 	for (int j = i; j < N; ++j) {
+// 	int pos = P.charAt(j) - 'a';
+// 	sum += Q.charAt(pos) - '0';
+// 	s += P.charAt(j);
+// 	if (sum <= K) {
+// 	S.add(s);}
+// 	else {
+// 	break;
+// 	}
+// 	}
+// 	}
+// 	return S.size();
+// 	}
+// 	public static void main(String[] args) {
+// 	Scanner sc = new Scanner(System.in);
+// 	String P = sc.nextLine();
+// 	String Q = sc.nextLine();
+// 	int K = sc.nextInt();
+// 	int N = P.length();
+// 	System.out.print(distinctSubString(P, Q, K, N));
+// 	}
+// 	}
